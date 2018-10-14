3de81a7fc3a23321b1f90cea3e11466458038208
unknowndatax <will_chang0928@hotmail.com>
2018-10-14 17:25:35 -0600

    AssemblyInfo update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Save_Editor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ys VIII: Lacrimosa of Dana Save Editor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Save_Editor")]
